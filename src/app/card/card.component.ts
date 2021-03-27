fbf780d5b54a8567ace05dc1bad5a25f0bce88cc
Иван <kleimenov416@gmail.com>
2021-03-27 11:25:40 -0400

    add come style to card component

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
-
 @Component({
-    selector: 'app-card',
-    templateUrl: './card.component.html'
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
 })
-
-export class CardComponent {
-
-}
+export class CardComponent {}
